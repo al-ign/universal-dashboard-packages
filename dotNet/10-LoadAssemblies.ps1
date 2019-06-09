a4f9d58acf21cddb479adb5225aae312b3cb0cc3
John Doe <33247544+al-ign@users.noreply.github.com>
2019-06-09 16:22:04 +0300

    added dotNET assemblies loading

---
+++ dotNet/10-LoadAssemblies.ps1
@@ -0,0 +1,20 @@
+﻿#this script will try to load any .dll assemblies in the Assemblies sub-folder in the dashboard root
+
+$assemblyLoadErrors = @()
+
+$cache:DashboardRootPath | Join-Path -ChildPath 'Assemblies' | Get-ChildItem -Filter '*.dll' -Recurse | % {
+    
+    $thisFilePath = $_.fullname
+    try {
+        [System.Reflection.Assembly]::LoadFile($_.fullname)
+        }
+    catch {
+        $assemblyLoadErrors += [pscustomobject]@{
+            File = $thisFilePath
+            Error = $Error[0]
+            }
+        }
+    }
+
+#can be used to debug
+$cache:assemblyLoadErrors = $assemblyLoadErrors
---
+++ dotNet/50-ListAssemblyLoadErrors.ps1
@@ -0,0 +1,32 @@
+﻿$PageTitle = 'Status dotNet Assemblies'
+
+$PageDescription = 'Display errors while loading dotNET assemblies'
+
+#Create UDPage Endpoint
+$scriptBlock = {
+    #Your content here
+    New-UDParagraph -Text (
+        '$cache:assemblyLoadErrors count: {0}' -f ($cache:assemblyLoadErrors).Count
+        )
+    
+    $cache:assemblyLoadErrors | % {
+        New-UDParagraph -Text (
+            '{0}' -f $_.File
+            )
+        New-UDParagraph -Text (
+            '{0}' -f $_.Error.Exception
+            )
+        }
+
+    #footer
+    New-UDParagraph -Text "This page was generated at $(Get-Date)"
+    } #End UDPage ScriptBlock
+
+
+#Create proper URL
+$PageUrl = '/{0}' -f $PageTitle -replace '\s'
+
+#Create UDPage variable
+New-Variable -Name ("UDPage" + $PageTitle -replace '\s') -Value (
+    New-UDPage -Url $PageUrl -Endpoint $scriptBlock
+    )
