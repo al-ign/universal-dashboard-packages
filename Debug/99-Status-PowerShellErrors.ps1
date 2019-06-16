470dc4fe6b2afd9854b62f37db6b3c878eba99a9
John Doe <33247544+al-ign@users.noreply.github.com>
2019-06-16 05:10:58 +0300

    Added a status page to view PowerShell

---
+++ Debug/99-Status-PowerShellErrors.ps1
@@ -0,0 +1,25 @@
+﻿$PageTitle = 'Status PowerShell Errors'
+
+#Create UDPage Content
+$scriptBlock = {
+    #Your content here
+    New-UDParagraph -Text ('$Error.count: {0}' -f $ArgumentList.Count )
+                
+    New-UDGrid -Title "Errors" -DefaultSortDescending -Endpoint {
+        $GridData = foreach ($thisArgument in $ArgumentList)  {
+            [pscustomobject]@{
+                Text = $thisArgument.Exception.Message
+                ScriptStackTrace = $thisArgument.ScriptStackTrace
+                }
+            }
+        $GridData | Out-UDGridData 
+        } -ArgumentList @($ArgumentList)
+
+    #footer
+    New-UDParagraph -Text "This page was generated at $(Get-Date)"
+    }
+        
+#Create UDPage variable
+New-Variable -Name ("UDPage" + $PageTitle -replace '\s') -Value (
+    New-UDPage -Name $PageTitle -Endpoint $scriptBlock  -ArgumentList @($Error)
+    )
